c76ce22ccc235d9e9700895926c6cd5a794165ca
sarveshpatil-git <patilsarveshgithub@gmail.com>
2026-07-14 19:37:43 +0530

    Add files via upload

---
+++ Sarveshpy/inputoutput.py
@@ -0,0 +1,14 @@
+#Normal string method for output
+
+name="Sarvesh Patil"
+age=20
+print("My name is :",name,"and my age is",age)
+
+#forbidden string method for output
+
+names="Sarvesh Patil"
+agee=20
+print(f"My name is {names} and my age is {agee}")
+
+sher=input("Todays weather was so romantic :")
+print(sher)
---
+++ Sarveshpy/logicalop.py
@@ -0,0 +1,10 @@
+#logical and or not
+
+print(123>100 and 500<900)
+print(123<100 and 500<900)
+
+print(123<100 or 500<900)
+print(123>100 or 500<900)
+
+print( not 500<900)
+print(not 12==12)
