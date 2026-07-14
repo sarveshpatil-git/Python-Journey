088b004d81fe2aff59ca301bade40b3c0fe1d534
sarveshpatil-git <patilsarveshgithub@gmail.com>
2026-07-14 20:05:40 +0530

    Add files via upload

---
+++ filehandlingpy/openfile.py
@@ -0,0 +1,2 @@
+file=open(r"C:\Users\win-10\OneDrive\Desktop\sarveshpy")
+print(file.read())
---
+++ filehandlingpy/writefile.py
@@ -0,0 +1,3 @@
+r=open("sumperman.txt",'a')
+r.write("Now i m appending some content in the file")
+r.close
